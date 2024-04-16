39261c66a0360068b06c29eb86f9881d341c06f0
Buddhasaravanan <saara1989@gmail.com>
2024-04-16 19:05:07 +0530

    project code started - April 16

--- eclipse-workspace/DTCloud_Cucumber/src/test/java/Pages/Project_Page.java
+++ eclipse-workspace/DTCloud_Cucumber/src/test/java/Pages/Project_Page.java
@@ -1,8 +1,14 @@
 package Pages;
 
+import java.io.IOException;
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+
+import factory.Base;
 
 public class Project_Page extends BasePage
 {
@@ -29,8 +35,7 @@ public class Project_Page extends BasePage
 	@FindBy(xpath="//span[contains (text(), 'Tasks')]") WebElement tasks;
 	@FindBy(xpath="(//a[contains (text(), 'create a new task')])[1]") WebElement createnewtask;
 	@FindBy(xpath="//input[@placeholder='New task']") WebElement taskname;
-	@FindBy(xpath="//mat-icon[@svgicon='calendarRange']") WebElement taskcalender;
-	//need to add task date 
+	@FindBy(xpath="//mat-icon[@svgicon='calendarRange']") WebElement taskcalender;	
 	@FindBy(xpath="//mat-icon[@svgicon='person']") WebElement resource;
 	@FindBy(xpath="//span[contains (text(),'Ajai DND')]") WebElement resource1;
 	@FindBy(xpath="//span[contains (text(),'Siva - DND')]") WebElement resource2;
@@ -43,12 +48,72 @@ public class Project_Page extends BasePage
 	@FindBy(xpath="(//mat-icon[@svgicon='addIcon'])[1]") WebElement addschedule;
 	@FindBy(xpath="//a[contains (text(), 'Assign Task')]") WebElement assigntask;
 	@FindBy(xpath="(//input[@type='checkbox'])[1]") WebElement taskcheckbox;
-	@FindBy(xpath="//span[contains (text(), 'Add selected tasks')]") WebElement addselectedtask;
+	@FindBy(xpath="//span[contains (text(), 'Add selected tasks')]") WebElement addselectedtask;	
+	@FindBy(xpath="//span[contains (text(), 'Time Tracking')]") WebElement timetracking;
+	@FindBy(id="btnTimeEntry") WebElement addnewtimeentry;
+	@FindBy(id="txt-app-search") WebElement addresource;
+	@FindBy(xpath="//div[contains (text(), 'Select project task')]") WebElement selectprojecttask;
+	@FindBy(xpath="(//span[contains (text(), 'Add')])[2]") WebElement addtimeentry;
+	@FindBy(xpath="//div[@class='time-container']") WebElement houresworked;
+	@FindBy(id="labor-thour-input") WebElement laborhour;
+	@FindBy(id="labor-tmin-input") WebElement labormins;
+	@FindBy(xpath="//input[@placeholder='Select Date(s)']") WebElement timeentrycalender;
+	@FindBy(xpath="//ng-select[@placeholder='Select labor type']") WebElement TElabortype;
+	@FindBy(xpath="//input[@type='checkbox']") WebElement itemcheckbox;
+	@FindBy(xpath="//span[contains (text(), 'Create purchase order')]") WebElement createpurchaseorder;
+	@FindBy(id="nav-product-status") WebElement itemstatus;
+	@FindBy(xpath="//span[contains (text(), ' New for all ')]") WebElement newforall;
+	@FindBy(xpath="//span[contains (text(), 'Continue')]") WebElement contunie;
+	@FindBy(xpath="//span[contains (text(), 'Send PO')]") WebElement sendpobutton;	
+	@FindBy(xpath="//mat-icon[@svgicon='closeIcon']") WebElement closeiconpo;
+	@FindBy(id="nav-payments") WebElement payments;
+	@FindBy(xpath="//a[contains (text(), 'New payment request')]") WebElement newpaymentrequest;
+	@FindBy(id="btn-done-automation-setup") WebElement createpayments;
+	@FindBy(xpath="//mat-icon[@svgicon='backIcon']") WebElement paymentbackicon;
+	@FindBy(xpath="//div[@aria-haspopup='menu']") WebElement paymentstatus;
+	@FindBy(xpath="(//span[contains (text(), ' Paid ')])[2]") WebElement paid;
+	@FindBy(xpath="//span[contains (text(), 'Save')]") WebElement paidstatussave;
 	
 	
 	
+	public void plan()
+	{
+		plan.click();
+	}
 	
+	public void task() throws InterruptedException
+	{
+		tasks.click();
+		Thread.sleep(2000);
+	}
 	
+	public void createtask() throws IOException, InterruptedException
+	{
+		createnewtask.click();
+		taskname.sendKeys(Base.getProperties().getProperty("Taskname"));
+		taskcalender.click();
+		
+		String Date ="10";
+		
+	List<WebElement>Alldates = driver.findElements(By.xpath("//*[@id=\"mat-datepicker-92\"]/div/mat-month-view/table/tbody"));
+	
+	for (WebElement dt:Alldates)
+	{
+		if(dt.getText().equals(Date))
+		{
+			dt.click();
+			break;
+		}
+	}
+	
+	resource.click();
+	Thread.sleep(2000);
+	resource1.click();
+	resource2.click();
+	assign.click();
+	create.click();
+		
+	}
 	
 	
 	
