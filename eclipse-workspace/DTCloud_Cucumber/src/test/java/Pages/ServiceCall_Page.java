921ee5964f7991cc1be1daa4275decfb85d33fe7
Buddhasaravanan <saara1989@gmail.com>
2024-11-26 18:52:12 +0530

    Nov 26

--- eclipse-workspace/DTCloud_Cucumber/src/test/java/Pages/ServiceCall_Page.java
+++ eclipse-workspace/DTCloud_Cucumber/src/test/java/Pages/ServiceCall_Page.java
@@ -19,6 +19,13 @@ public class ServiceCall_Page extends BasePage
 	@FindBy(id="app-nav-service-management-orders") WebElement calls;
 	@FindBy(xpath="//span[contains (text(), 'Schedule service')]") WebElement createservicecall;
 	@FindBy(xpath="//input[@name='client']") WebElement clientname;
+	@FindBy(xpath="//div[contains (text(), 'E.g. Customer called into office on Tuesday stated their conference room projector stopped working.')]") WebElement issuereported;
+	@FindBy(xpath="//span[contains (text(), 'Done')]") WebElement issuereporteddone;
+	@FindBy(xpath="(//span[contains (text(), 'Create')])[2]]") WebElement createSC;
+	@FindBy(xpath="//input[@placeholder='Select start date']") WebElement SCstartdate;
+	@FindBy(xpath="//input[@placeholder='Select end date']") WebElement SCenddate;	
+	@FindBy(xpath="(//div[@role='combobox'])[4]") WebElement servicecontractdropbox;
+	@FindBy(xpath="//span[contains (text(), 'Request payment')]") WebElement requestpayment;
 	
 	
 	
